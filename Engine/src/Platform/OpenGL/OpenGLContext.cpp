18f4fa6c93e272e5f21a94c9f27c0b14c3d7d6d7
iojuedioe9rd <catsstudio81@gmail.com>
2024-08-06 16:51:18 +0100

    Added check for OpenGL version

--- Engine/src/Platform/OpenGL/OpenGLContext.cpp
+++ Engine/src/Platform/OpenGL/OpenGLContext.cpp
@@ -22,6 +22,15 @@ namespace Vertex
 		VX_CORE_INFO("  Renderer: {0}", (const char*)glGetString(GL_RENDERER));
 		VX_CORE_INFO("  Version: {0}", (const char*)glGetString(GL_VERSION));
 
+#ifdef VX_ENABLE_ASSERTS
+		int versionMajor;
+		int versionMinor;
+		glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+		glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+		VX_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "Vertex requires at least OpenGL version 4.5!");
+#endif
+
 		
 	}
 
